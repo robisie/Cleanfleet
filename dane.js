1ad1fac067fc97ce9f3ee2b2e869e8c0e94cd917
robisie <robisie-fer@o2.pl>
2026-08-26 20:42:25 +0200

    Add files via upload

---
+++ dane.js
@@ -0,0 +1,534 @@
+// CleanFleet — centralna baza pojazdów
+// Ten plik zawiera wyłącznie bazę pojazdów. Wpisy prania pozostają w bazie centralnej.
+window.FLEET_DATA = {
+  "registry": {
+    "ST7765X": {
+      "typ": "SOLÓWKA",
+      "marka": "VOLVO"
+    },
+    "ST9361S": {
+      "typ": "SOLÓWKA",
+      "marka": "SCANIA"
+    },
+    "WPR9486N": {
+      "typ": "ZESTAW",
+      "marka": "VOLVO"
+    },
+    "SB935EY": {
+      "typ": "BUS",
+      "marka": "----"
+    },
+    "SK9E312": {
+      "typ": "BUS",
+      "marka": "----"
+    },
+    "ST7187X": {
+      "typ": "BUS",
+      "marka": "----"
+    },
+    "ST9828X": {
+      "typ": "BUS",
+      "marka": "----"
+    },
+    "ST1920W": {
+      "typ": "ZESTAW",
+      "marka": "SCANIA"
+    },
+    "WGM3683C": {
+      "typ": "SOLÓWKA",
+      "marka": "SCANIA"
+    },
+    "WGM6790A": {
+      "typ": "ZESTAW",
+      "marka": "SCANIA"
+    },
+    "WGM3354C": {
+      "typ": "SOLÓWKA",
+      "marka": "SCANIA"
+    },
+    "SB148FE": {
+      "typ": "BUS",
+      "marka": "----"
+    },
+    "ST9808V": {
+      "typ": "SOLÓWKA",
+      "marka": "SCANIA"
+    },
+    "ST9362S": {
+      "typ": "SOLÓWKA",
+      "marka": "SCANIA"
+    },
+    "SB253FU": {
+      "typ": "BUS",
+      "marka": "----"
+    },
+    "SB254FU": {
+      "typ": "BUS",
+      "marka": "----"
+    },
+    "SB255FU": {
+      "typ": "BUS",
+      "marka": "----"
+    },
+    "SB258EK": {
+      "typ": "BUS",
+      "marka": "----"
+    },
+    "ST9354S": {
+      "typ": "SOLÓWKA",
+      "marka": "SCANIA"
+    },
+    "DTV576": {
+      "typ": "SOLÓWKA",
+      "marka": "SCANIA"
+    },
+    "SK870UW": {
+      "typ": "OSOBOWY",
+      "marka": "----"
+    },
+    "WGM6792A": {
+      "typ": "ZESTAW",
+      "marka": "SCANIA"
+    },
+    "ST3701T": {
+      "typ": "SOLÓWKA",
+      "marka": "MAN"
+    },
+    "ST9164X": {
+      "typ": "SOLÓWKA",
+      "marka": "VOLVO"
+    },
+    "WGM5718J": {
+      "typ": "ZESTAW",
+      "marka": "SCANIA"
+    },
+    "ST9587S": {
+      "typ": "ZESTAW",
+      "marka": "MAN"
+    },
+    "WGM84690": {
+      "typ": "ZESTAW",
+      "marka": "VOLVO"
+    },
+    "ST8959P": {
+      "typ": "SOLÓWKA",
+      "marka": "MAN"
+    },
+    "ST3205T": {
+      "typ": "SOLÓWKA",
+      "marka": "SCANIA"
+    },
+    "DTV571": {
+      "typ": "SOLÓWKA",
+      "marka": "SCANIA"
+    },
+    "ST9590S": {
+      "typ": "ZESTAW",
+      "marka": "MAN"
+    },
+    "ST0318W": {
+      "typ": "SOLÓWKA",
+      "marka": "MAN"
+    },
+    "WGM3697C": {
+      "typ": "SOLÓWKA",
+      "marka": "SCANIA"
+    },
+    "WGM2182C": {
+      "typ": "SOLÓWKA",
+      "marka": "SCANIA"
+    },
+    "ST4855X": {
+      "typ": "BUS MŁYNCZAK",
+      "marka": "----"
+    },
+    "WE3AS93": {
+      "typ": "BUS",
+      "marka": "----"
+    },
+    "WGM2185C": {
+      "typ": "SOLÓWKA",
+      "marka": "SCANIA"
+    },
+    "ST5130P": {
+      "typ": "ZESTAW",
+      "marka": "VOLVO"
+    },
+    "WE2AT65": {
+      "typ": "BUS",
+      "marka": "----"
+    },
+    "WGM2183C": {
+      "typ": "SOLÓWKA",
+      "marka": "SCANIA"
+    },
+    "WGM2184C": {
+      "typ": "SOLÓWKA",
+      "marka": "SCANIA"
+    },
+    "ST8803S": {
+      "typ": "ZESTAW",
+      "marka": "MAN"
+    },
+    "WGM9832F": {
+      "typ": "",
+      "marka": "----"
+    },
+    "WGM3684C": {
+      "typ": "SOLÓWKA",
+      "marka": "SCANIA"
+    },
+    "WGM6786A": {
+      "typ": "SOLÓWKA",
+      "marka": "----"
+    },
+    "ST9366S": {
+      "typ": "",
+      "marka": "----"
+    },
+    "ST9681S": {
+      "typ": "",
+      "marka": "----"
+    },
+    "WGM6785A": {
+      "typ": "",
+      "marka": "----"
+    },
+    "WGM6787A": {
+      "typ": "",
+      "marka": "----"
+    },
+    "WGM6782A": {
+      "typ": "ZESTAW",
+      "marka": "----"
+    },
+    "WGM6789A": {
+      "typ": "ZESTAW",
+      "marka": "----"
+    },
+    "WGM7136J": {
+      "typ": "",
+      "marka": "----"
+    },
+    "SPS72699": {
+      "typ": "OSOBOWE",
+      "marka": "RENAULT"
+    },
+    "WGM3698C": {
+      "typ": "SOLÓWKA",
+      "marka": "SCANIA"
+    },
+    "ST2943P": {
+      "typ": "SOLÓWKA",
+      "marka": "VOLVO"
+    },
+    "WGM85721": {
+      "typ": "SOLÓWKA",
+      "marka": "VOLVO"
+    },
+    "SK920SN": {
+      "typ": "SOLÓWKA",
+      "marka": "MAN"
+    },
+    "SK495XP": {
+      "typ": "BUS MŁYNCZAK",
+      "marka": "----"
+    },
+    "ST3874W": {
+      "typ": "BUS MŁYNCZAK",
+      "marka": "----"
+    },
+    "SK064YK": {
+      "typ": "BUS MŁYNCZAK",
+      "marka": "----"
+    },
+    "SB036HP": {
+      "typ": "BUS",
+      "marka": "----"
+    },
+    "ST4142X": {
+      "typ": "BUS",
+      "marka": "----"
+    },
+    "WGM84691": {
+      "typ": "ZESTAW",
+      "marka": "----"
+    },
+    "WGM6788A": {
+      "typ": "ZESTAW",
+      "marka": "----"
+    },
+    "ST7405X": {
+      "typ": "BUS",
+      "marka": "----"
+    },
+    "WE2AC89": {
+      "typ": "BUS",
+      "marka": "----"
+    },
+    "DTV578": {
+      "typ": "SOLÓWKA",
+      "marka": "SCANIA"
+    },
+    "WGM9830F": {
+      "typ": "SOLÓWKA",
+      "marka": "----"
+    },
+    "ST7761X": {
+      "typ": "SOLÓWKA",
+      "marka": "VOLVO"
+    },
+    "ST7762X": {
+      "typ": "SOLÓWKA",
+      "marka": "VOLVO"
+    },
+    "SK7G395": {
+      "typ": "",
+      "marka": "----"
+    },
+    "SK8G491": {
+      "typ": "",
+      "marka": "----"
+    },
+    "SK8G492": {
+      "typ": "",
+      "marka": "----"
+    },
+    "ST4128X": {
+      "typ": "",
+      "marka": "----"
+    },
+    "ST4130X": {
+      "typ": "",
+      "marka": "----"
+    },
+    "ST7186X": {
+      "typ": "",
+      "marka": "----"
+    },
+    "ST7314X": {
+      "typ": "",
+      "marka": "----"
+    },
+    "ST7683X": {
+      "typ": "",
+      "marka": "----"
+    },
+    "ST7763X": {
+      "typ": "",
+      "marka": "----"
+    },
+    "ST7764X": {
+      "typ": "",
+      "marka": "----"
+    },
+    "ST9163X": {
+      "typ": "",
+      "marka": "----"
+    },
+    "ST6424U": {
+      "typ": "SOLÓWKA",
+      "marka": "SCANIA"
+    },
+    "ST6389U": {
+      "typ": "",
+      "marka": "----"
+    },
+    "ST6425U": {
+      "typ": "",
+      "marka": "----"
+    },
+    "ST6427U": {
+      "typ": "",
+      "marka": "----"
+    },
+    "WGM9831F": {
+      "typ": "",
+      "marka": "----"
+    },
+    "WGM6791A": {
+      "typ": "ZESTAW",
+      "marka": "SCANIA"
+    },
+    "ST9355S": {
+      "typ": "SOLÓWKA",
+      "marka": "SCANIA"
+    },
+    "ST9364S": {
+      "typ": "SOLÓWKA",
+      "marka": "SCANIA"
+    },
+    "WGM3682C": {
+      "typ": "SOLÓWKA",
+      "marka": "SCANIA"
+    },
+    "WGM3685C": {
+      "typ": "SOLÓWKA",
+      "marka": "----"
+    },
+    "SK305TY": {
+      "typ": "",
+      "marka": "----"
+    },
+    "ST3702T": {
+      "typ": "",
+      "marka": "----"
+    },
+    "ST3703T": {
+      "typ": "",
+      "marka": "----"
+    },
+    "ST3751T": {
+      "typ": "",
+      "marka": "----"
+    },
+    "ST3753T": {
+      "typ": "",
+      "marka": "----"
+    },
+    "ST8815S": {
+      "typ": "",
+      "marka": "----"
+    },
+    "ST9365S": {
+      "typ": "",
+      "marka": "----"
+    },
+    "ST9583S": {
+      "typ": "",
+      "marka": "----"
+    },
+    "ST9586S": {
+      "typ": "",
+      "marka": "----"
+    },
+    "WGM2181C": {
+      "typ": "",
+      "marka": "----"
+    },
+    "WGM3353C": {
+      "typ": "",
+      "marka": "----"
+    },
+    "WGM3357C": {
+      "typ": "",
+      "marka": "----"
+    },
+    "WGM3696C": {
+      "typ": "",
+      "marka": "----"
+    },
+    "WGM3699C": {
+      "typ": "",
+      "marka": "----"
+    },
+    "WGM6783A": {
+      "typ": "",
+      "marka": "----"
+    },
+    "WGM6784A": {
+      "typ": "",
+      "marka": "----"
+    },
+    "WGM3356C": {
+      "typ": "",
+      "marka": "----"
+    },
+    "SK446SV": {
+      "typ": "SOLÓWKA",
+      "marka": "MAN"
+    },
+    "SK773SV": {
+      "typ": "SOLÓWKA",
+      "marka": "MAN"
+    },
+    "SK335SV": {
+      "typ": "",
+      "marka": "----"
+    },
+    "SK455SL": {
+      "typ": "",
+      "marka": "----"
+    },
+    "WGM85715": {
+      "typ": "",
+      "marka": "----"
+    },
+    "WGM85716": {
+      "typ": "",
+      "marka": "----"
+    },
+    "WGM85717": {
+      "typ": "",
+      "marka": "----"
+    },
+    "WGM85718": {
+      "typ": "",
+      "marka": "----"
+    },
+    "WGM85719": {
+      "typ": "",
+      "marka": "----"
+    },
+    "WGM85720": {
+      "typ": "",
+      "marka": "----"
+    },
+    "WPR9485N": {
+      "typ": "",
+      "marka": "----"
+    },
+    "SK813UY": {
+      "typ": "BUS MŁYNCZAK",
+      "marka": "----"
+    },
+    "SB253EK": {
+      "typ": "BUS",
+      "marka": "----"
+    },
+    "SB603HJ": {
+      "typ": "BUS",
+      "marka": "----"
+    },
+    "SK232SA": {
+      "typ": "",
+      "marka": "----"
+    },
+    "ST2475R": {
+      "typ": "",
+      "marka": "----"
+    },
+    "ST2629R": {
+      "typ": "",
+      "marka": "----"
+    },
+    "ST2650R": {
+      "typ": "",
+      "marka": "----"
+    },
+    "ST5132P": {
+      "typ": "",
+      "marka": "----"
+    },
+    "ST8960P": {
+      "typ": "",
+      "marka": "----"
+    },
+    "WGM74937": {
+      "typ": "",
+      "marka": "----"
+    },
+    "SB522EY": {
+      "typ": "BUS",
+      "marka": "----"
+    },
+    "SB732EH": {
+      "typ": "BUS",
+      "marka": "----"
+    },
+    "ST572AJ": {
+      "typ": "BUS",
+      "marka": "----"
+    }
+  }
+};

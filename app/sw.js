6c838deccec486634d5cc5419279a45cfc300dcd
robisie <robisie-fer@o2.pl>
2026-09-15 10:59:21 +0200

    Bump weather placement script version

--- app/sw.js
+++ app/sw.js
@@ -57,7 +57,7 @@ self.addEventListener('fetch', event => {
     html = html.replace(/<script src="\/app\/weather\.js\?v=[^"]+"><\/script>/g, '');
     html = html.replace(/<script src="\/app\/weather-position\.js\?v=[^"]+"><\/script>/g, '');
 
-    const weatherScript = '<script src="/app/weather.js?v=20260915-2"></script>\n<script src="/app/weather-position.js?v=20260915-1"></script>';
+    const weatherScript = '<script src="/app/weather.js?v=20260915-2"></script>\n<script src="/app/weather-position.js?v=20260915-2"></script>';
     if (html.includes('</body>')) {
       html = html.replace('</body>', `${weatherScript}\n</body>`);
     } else {
